8d825b19e31bf3d04bc4877e829d728458d5596c
Azeddine Hamdaoui <azeddine.hmd@gmail.com>
2021-10-01 10:00:55 +0100

    ctrl_l_triggered remember last buffered command

--- input_key_second.c
+++ input_key_second.c
@@ -1,9 +1,16 @@
+#include "libx/libx.h"
 #include "minishell.h"
 
 void	ctrl_l_triggered(t_termarg *targ)
 {
+	char	*saved_buf_str;
+
 	//TODO: execute shell command without system function
 	system("clear");
+	saved_buf_str = xstrdup(targ->buf->str);
 	ms_bufrst(targ->buf);
 	ms_prompt();
+	ms_bufrpc(targ->buf, saved_buf_str);
+	ft_putstr(targ->buf->str);
+	xfree(saved_buf_str);
 }
