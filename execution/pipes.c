0649036963ba733ca075d512e59ea9c613ba7bbc
BooDeer <hsissi3a@gmail.com>
2021-10-13 15:55:21 +0100

    saving up latest pipe.c stable version

--- execution/pipes.c
+++ execution/pipes.c
@@ -6,11 +6,11 @@
 /*   By: boodeer <boodeer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/21 15:16:10 by hboudhir          #+#    #+#             */
-/*   Updated: 2021/10/07 19:42:52 by boodeer          ###   ########.fr       */
+/*   Updated: 2021/10/13 15:45:20 by boodeer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#include "execution.h"
 int			close_all(int input, int fd1, int fd2, int pos)
 {
 	if (input)
@@ -24,40 +24,45 @@ int			close_all(int input, int fd1, int fd2, int pos)
 
 // Equal to next->id (minishell_240)
 
-int		dup_fd(t_cmd *cmd, int fds[2], int input, int fd_zero)
+int		dup_fd(t_list *cmds, int fds[2], int input, int fd_zero)
 {
+	t_cmd	*cmd;
+	t_cmd	*next;
+
+	cmd = (t_cmd*)cmds->content;
+	next = (t_cmd*)cmds->next->content;
 	input++;
-	if (cmd->is_piped == 0 && cmd->next->is_piped == 1) // id = 1
+	if (cmd->is_piped == 0 && next->is_piped == 1) // id = 1
 		dup2(fds[1], 1);
-	else if (cmd->is_piped && cmd->next && cmd->next->is_piped) // id = 2
+	else if (cmd->is_piped && cmds->next && next->is_piped) // id = 2
 	{
 		dup2(fd_zero, 0);
 		dup2(fds[1], 1);
 	}
 	else													// id = 
 		dup2(fd_zero, 0);
-	if (cmd->is_piped && cmd->next && cmd->next->is_piped)
+	if (cmd->is_piped && cmds->next && next->is_piped)
 		return(2);
 	return (0);
 }
 
-t_cmd			*pipes(t_cmd *cmd)
+t_cmd			*pipes(t_list *cmds)
 {
-	t_cmd 	*head;
+	t_cmd	*cmd;
 	int		fds[2];
 	int		pid;
 	int		input  = 0;
 	int		pos = 0;
 	int		fd_zero = 0;
-	head = cmd;
-	while (cmd)
+	
+	while (cmds)
 	{
-		//if (cmd->next) // (?)
+		cmd = (t_cmd*)cmds->content;
 		pipe(fds);
 		pid = fork();
 		if (!pid)
 		{
-			pos = dup_fd(cmd, fds, input, fd_zero);
+			pos = dup_fd(cmds, fds, input, fd_zero);
 			close(fds[1]);
 			close(fds[0]);
 			close(fd_zero);
@@ -67,14 +72,14 @@ t_cmd			*pipes(t_cmd *cmd)
 			close(fd_zero);
 		fd_zero = fds[0];
 		//input = close_all(input, fds[0], fds[1], pos); must check before delete
-		if (!cmd->next || !cmd->next->is_piped)
+		if (!cmds->next || !((t_cmd*)cmds->next->content)->is_piped)
 			break ;
-		cmd = cmd->next;
+		cmds = cmds->next;
 		close(fds[1]);
 	}
 	while (wait(&pid) != -1)
 		;
 	if (WIFEXITED(pid))
-		cmd->err = WEXITSTATUS(pid);
-	return (cmd);
+		cmd->ret = WEXITSTATUS(pid);
+	return (cmds);
 }
