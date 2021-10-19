e2a7591c99e1783d2d18fff99c6162c9b6383fed
Azeddine Hamdaoui <40150482+azeddine-hmd@users.noreply.github.com>
2021-10-19 23:01:21 +0100

    Revert "Revert "B exec""

--- execution/main.c
+++ execution/main.c
@@ -1,4 +1,4 @@
-#include "minishell.h"
+#include "execution.h"
 # define true 1
 
 
@@ -14,230 +14,196 @@ int			cmd_nfound(char *str)
 {
 	write(2, "minishell: ", 11);
 	write(2, str, ft_strlen(str));
-	write(2,": command not found\n", 19);
+	write(2,": command not found\n", 20);
 	return (127);
 }
 
-int			exec_builtin(char **cmd) // ["cd", ...]
+int			exec_builtin(char **cmd, char ***env) // ["cd", ...]
 {
-	if (!ft_strcmp(cmd[0], "echo"))
-		return (ft_builtin_echo(cmd + 1));
-	else if (!ft_strcmp(cmd[0], "cd"))
-		return (ft_builtin_cd(cmd + 1));
-	else if (!ft_strcmp(cmd[0], "pwd"))
+	if (!ft_strcmp(cmd[0], "echo")) // no memleak
+		return (ft_builtin_echo(cmd));
+	else if (!ft_strcmp(cmd[0], "cd")) // memleak
+		return (ft_builtin_cd(cmd + 1, *env));
+	else if (!ft_strcmp(cmd[0], "pwd")) // no memleak
 		return (ft_builtin_pwd());
-	else if (!ft_strcmp(cmd[0], "export"))
-		return (ft_builtin_export(cmd + 1));
-	else if (!ft_strcmp(cmd[0], "unset"))
-		return (ft_builtin_unset(cmd + 1));
-	else if (!ft_strcmp(cmd[0], "env"))
-		return (ft_builtin_env());
-	else if (!ft_strcmp(cmd[0], "exit"))
+	else if (!ft_strcmp(cmd[0], "export")) // not working
+		return (ft_builtin_export(cmd + 1, env));
+	else if (!ft_strcmp(cmd[0], "unset")) // not working
+		return (ft_builtin_unset(cmd + 1, env));
+	else if (!ft_strcmp(cmd[0], "env")) // no memleak
+		return (ft_builtin_env(*env));
+	else if (!ft_strcmp(cmd[0], "exit")) // not working
 		return (ft_builtin_exit(10));
 	return (2);
 }
 
-int			run_cmd(char *exec_path, char **args)
+int			run_cmd(char *exec_path, char **args, char **env)
 {
 	pid_t	pid;
 
 	pid = fork();
 	if (pid == 0)
-		execve(exec_path, args, g_env);
+		execve(exec_path, args, env);
 	else if (pid < 0)
 	{
-		free(exec_path);
-		write(1, "Failed to create fork\n", ft_strlen("Failed to create fork\n"));
-		return (-1);
-	}
-	wait(&pid);
-	if (exec_path)
-		free(exec_path);
-	return (1);
-}
-
-int			is_exec(char *exec_path, struct stat stats, char **cmd) // check better this function
-{
-	if (stats.st_mode & S_IFREG)
-	{
-		if (stats.st_mode & S_IXUSR)
-			return (run_cmd(exec_path, cmd));
-		else
-			error_msg("Permission denied!\n", 2, 0);
-		free(exec_path);
+		write(2, "Failed to create fork\n", ft_strlen("Failed to create fork\n"));
 		return (1);
 	}
-	free(exec_path);
-	return (0);
-}
-
-int			exec_bin(char **cmd)		// equal to check_bin();
-{
-	char	**path = ft_split(getenv("PATH"), ':');
-	int		i;
-	char	*exec_path;
-	struct stat stats;
-
-	i = -1;
-	while (path && path[++i])
-	{
-		if (ft_strstartw(cmd[0], path[i]))
-			exec_path = ft_strdup(cmd[0]);
-		else
-			exec_path = ft_pathjoin(path[i], cmd[0]);
-		if (lstat(exec_path, &stats) == -1)
-			free(exec_path);
-		else
-		{
-			ft_freestrarr(path);
-			return (is_exec(exec_path, stats, cmd));
-		}
-	}
-	ft_freestrarr(path);
-	return (0);
+	wait(&pid);
+		return ((unsigned char)pid);
 }
 
 
-
-
-
-void		append_lst(t_cmd **cmd, t_cmd *src)
-{
-	t_cmd	*tmp;
-
-	if (*cmd == NULL)
-		*cmd = src;
-	else
-	{
-		tmp = *cmd;
-		while (tmp->next != NULL)
-			tmp = tmp->next;
-		tmp->next = src;
-	}
-}
-
-t_cmd		*fill_dummy(t_cmd **cmd, char **cmd_0, char **cmd_1)
-{
-	int		i;
-	t_cmd	*dummy;
-	i = 0;
-	while (i < 2)
-	{
-		dummy = (t_cmd *)malloc(sizeof(t_cmd));
-		dummy->pos = i;
-		switch (i)
-		{
-		case 0:
-			dummy->is_piped = 0;
-			dummy->previous = NULL;
-			dummy->next = NULL;
-			dummy->args = cmd_0;
-			break;
-		case 1:
-			dummy->is_piped = 1;
-			dummy->next = NULL;
-			dummy->args = cmd_1;
-		default:
-			break;
-		}
-		append_lst(cmd ,dummy);
-		i++;
-	}
-	return (*cmd);
-}
-
-void		append_lstk(t_token **cmd, t_token *src)
-{
-	t_token	*tmp;
-
-	if (*cmd == NULL)
-		*cmd = src;
-	else
-	{
-		tmp = *cmd;
-		while (tmp->next != NULL)
-			tmp = tmp->next;
-		tmp->next = src;
-	}
-}
-
-/*
-*
-*	arr ==> ["main.c", "test.c", NULL]
-*	type ==> "aowwwa"  a = append, o = override,  w = write
-*/
-/* 
-====
-*/
-void		fill_token(t_token **head, char **arr, char* type)
-{
-
-	t_token  *token;
-	int			i = -1;
-
-	while (arr[++i])
-	{
-		token = (t_token *)malloc(sizeof(t_token));
-		token->value = ft_strdup(arr[i]);
-		token->type = type[i];
-		token->next = NULL;
-		append_lstk(head, token);
-	}
-}
-
-void		output_token(t_token *token)
-{
-	while(token)
-	{
-		printf("%s====>%c\n", token->value, token->type);
-		token = token->next;
-	}
-}
-
-int			check_cmd(t_cmd *cmd)
+int			execute_p(char *p, char **cmd, char **env)
 {
 	int		ret;
-	int		i;
 
 	ret = 0;
+	if (open(p, O_RDONLY) <= 0)
+	{
+		free(p);
+		return (127);
+	}
+	else
+	{
+		ret = run_cmd(p, cmd, env);
+		if (ret)
+		{
+			free(p);
+			return (ret);
+		}
+	}
+	free(p);
+	return (ret);
+}
+
+int			exec_path(char **cmd, char **env)
+{
+	char	*tmp;
+	int		ret;
+
+	if (cmd[0][0] == '.')
+	{
+		tmp = ft_strjoin(getcwd(NULL, 0), "/");
+		ret = execute_p(ft_strjoin(tmp, cmd[0]), cmd, env);
+		free(tmp);
+		if (ret == 127)
+			ret = 2;
+		return (ret);
+	}
+	if (cmd[0][0] == '/')
+	{
+		ret = execute_p(ft_strdup(cmd[0]), cmd, env);
+		if (ret == 127)
+			ret = 2;
+		return (ret);
+	}
+	return (0);
+}
+
+char		**split_path(char **env)
+{
+	char	*tmp;
+	char	**path;
+
+	tmp = ft_substr(find_strenv("PATH", env), 5, ft_strlen(find_strenv("PATH", env))); // check if num 5 is good enough
+	path = ft_split(tmp, ':');
+	free(tmp);
+	return (path);
+}
+
+int			exec_bin(char **cmd, char **env)		// equal to check_bin();
+{
+	char **path;
+	int	i;
+	int	ret;	
+
 	i = -1;
-	if (!cmd) // see the difference between this and the condition below
+	ret = 0;
+	if (cmd[0][0] == '.' || cmd[0][0] == '/')
+			return (exec_path(cmd, env));
+	path = split_path(env);
+	if (!path)
+		return (2);
+	while (path[++i])
+	{
+		ret = execute_p(ft_pathjoin(path[i], cmd[0]), cmd, env);
+		if (!ret)
+			break ;
+	}
+	ft_freestrarr(path);
+	return (ret);
+}
+
+ int	file_error(char *s)
+{
+	write(2, "minishell: ", 11);
+	write(2, s, strlen(s));
+	write(2, ": No such file or directory\n", 28);
+	return (127);
+}
+
+int			check_cmd(t_cmd *cmd, char ***env)
+{
+	int		ret;
+	FILE *fd = fopen("/tmp/pipe_debugging", "a+");
+	ret = 0;
+	if (!cmd)
 		return (0);
 	if (cmd->args[0][0] != '\0')
 	{
-		ret = exec_builtin(cmd->args); // changes this line [04/10/21]
+		ret = exec_builtin(cmd->args, env); // changes this line [04/10/21] edit1: there's memory leak here
 		if (ret != 2)
+		{
+			fprintf(fd, "Return value of the command: %s is: %d\n", cmd->args[0], ret);
+			fflush(fd);
 			return (ret);
-		if (find_strenv("PATH")[0] == '\0')
+		}
+		if (!find_strenv("PATH", *env)[0])
 			ret = 2;
 		else
-			ret = exec_bin(cmd->args);
+		{
+			ret = exec_bin(cmd->args, *env);
+			if (ret != 2 && ret != 127)
+			{
+				fprintf(fd, "Return value of the command: %s is: %d\n", cmd->args[0], ret);
+				fflush(fd);
+				return (ret);
+			}
+		}
+		if (ret == 2)
+		{
+			fprintf(fd, "Return value of the command: %s is: %d\n", cmd->args[0], ret);
+			fflush(fd);
+			return (file_error(cmd->args[0]));
+		}
 	}
 	//if (ret == 127)
 		//return (file_dont_exist(cmd->args[0]));
+	fprintf(fd, "Return value of the command: %s is: %d\n", cmd->args[0], ret);
+	fflush(fd);
 	return (cmd_nfound(cmd->args[0]));
 }
 
-int			exec_cmd(t_cmd *cmd, t_token *token)
+int			exec_cmd(t_cmd* cmd, char ***env)
 {
 	int		fd_in;
 	int		fd_out;
 	int		ret;
 
 	ret = 0;
-	if (cmd->in_token->content || cmd->out_token->content)
+	if (cmd->in_token || cmd->out_token)
 	{
 		fd_in = dup(0);
 		fd_out = dup(1);
-	}
-
-	if (cmd->in_token->content || cmd->out_token->content)
-		if (redirections(token))
+		if (redirections(cmd))
 			return (1);
-	ret = check_cmd(cmd);
-	//ret = 
-	if (cmd->in_token->content || cmd->out_token->content)
-	{
+	}
+		
+	ret = check_cmd(cmd, env);
+	////ret = 
+	if (cmd->in_token || cmd->out_token)
+	{	
 		dup2(fd_in, 0);
 		dup2(fd_out, 1);
 		close(fd_in);
@@ -246,42 +212,42 @@ int			exec_cmd(t_cmd *cmd, t_token *token)
 	return (ret);
 }
 
-int			main_function(t_cmd *cmd, t_token *token)
+int			main_function(t_list *cmds, char ***env)
 {
 	int		r;
 
 	r = 0;
 	// TODO:
 	// check filter_all in the other folder;
-	while (cmd)
+	while (cmds)
 	{
-		if (cmd->is_piped)
-			cmd = pipes(cmd);
+		if (cmds->next)
+			cmds = pipes(cmds, env); // done
 		else
-			r = exec_cmd(cmd, token);
-		cmd = cmd->next;
+			r = exec_cmd(((t_cmd*)cmds->content), env);
+		cmds = cmds->next;
 	}
 	return (r);
 }
 
-int			main(int argc, char **argv, char **env)
-{
-	//printf("\n\n");
-	//t_cmd	*test = NULL;					// testing linked ist
-	t_token *token = NULL; 
-	//char	*red[] =  {"test.c", "test.txt", "test", NULL};
-	//fill_token(&token, red, "aca");
-	//output_token(token);
-	init_env(argc, argv, env);
-	char	*cmd[] = {"echo","hello", NULL};
-	//char	*cmd1[] = {"wc",NULL};
-	//exec_builtin(cmd1);
-	//redirections(token); // in the exec_func dup fd_in and fd_out before opening redirections
-	exec_bin(cmd);	
-	//printf("%s\n", getenv("PWD"));
-	//char	*cmd2[] = {"cd",NULL};
-	//exec_builtin(cmd2);
-	//test = fill_dummy(&test, cmd, cmd1);
-	//pipes(test);
-	return (0);
-}
+//int			main(int argc, char **argv, char **env)
+//{
+//	//printf("\n\n");
+//	//t_cmd	*test = NULL;					// testing linked ist
+//	t_token *token = NULL; 
+//	//char	*red[] =  {"test.c", "test.txt", "test", NULL};
+//	//fill_token(&token, red, "aca");
+//	//output_token(token);
+	//init_env(argc, argv, env);
+//	char	*cmd[] = {"echo","hello", NULL};
+//	//char	*cmd1[] = {"wc",NULL};
+//	//exec_builtin(cmd1);
+//	//redirections(token); // in the exec_func dup fd_in and fd_out before opening redirections
+//	exec_bin(cmd);	
+//	//printf("%s\n", getenv("PWD"));
+//	//char	*cmd2[] = {"cd",NULL};
+//	//exec_builtin(cmd2);
+//	//test = fill_dummy(&test, cmd, cmd1);
+//	//pipes(test);
+//	return (0);
+//}
--- execution/unset.c
+++ execution/unset.c
@@ -3,48 +3,48 @@
 /*                                                        :::      ::::::::   */
 /*   unset.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hboudhir <hboudhir@student.42.fr>          +#+  +:+       +#+        */
+/*   By: boodeer <boodeer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/09 10:58:45 by hboudhir          #+#    #+#             */
-/*   Updated: 2021/06/09 10:59:04 by hboudhir         ###   ########.fr       */
+/*   Updated: 2021/10/18 20:44:22 by boodeer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#include "execution.h"
 
-void		delete_env(int	index)
+char		**delete_env(int	index, char **env)
 {
 	int		i;
 	int		j;
 
-	free(g_env[index]);
-	g_env[index] = NULL;
+	free(env[index]);
+	env[index] = NULL;
 	i = index;
 	j = index + 1;
-	while (g_env[i + 1])
+	while (env[i + 1])
 	{
-		g_env[i] = ft_strdup(g_env[i + 1]);
-		free(g_env[i + 1]);
+		env[i] = ft_strdup(env[i + 1]);
+		free(env[i + 1]);
 		i++;
 		j++;
 	}
-	g_env = realloc_env(j - 1);
+	env = realloc_env(j - 1, env);
+	return (env);
 }
 
-int			ft_builtin_unset(char **args)
+int			ft_builtin_unset(char **args, char ***env)
 {
 	int		i;
 	int		index;
 
-	//printf("%s\n\n", args[0]);
 	if (!args[0])
-		return(error_msg("Error!\n too few arguments.", 2, 1));
+		return(error_msg("Error!\n too few arguments.\n", 2, 1));
 	i = -1;
 	while (args[++i])
 	{
-		index	= find_env(args[i]);
-		if (g_env[index])
-			delete_env(index);
+		index	= find_env(args[i], *env);
+		if (env[index])
+			*env = delete_env(index, *env);
 	}
 	return (1);
 }
