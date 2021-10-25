27c50a7f92455504a620b072c7f3e09999400862
Azeddine hamdaiou <azeddine.hmd@gmail.com>
2021-10-25 17:39:33 +0100

    adding condigtion for child and heredoc signal

--- signal_interceptor.c
+++ signal_interceptor.c
@@ -2,7 +2,16 @@
 
 void	signal_interceptor(int sig)
 {
-	//TODO: impelment
-	(void)sig;
-	print_all_signal();
+	if (sig == SIGINT)
+	{
+		print_all_signal();
+		if (g_sign.heredoc_running)
+		{
+			//TODO: implmenet
+		}
+		else if (g_sign.child_running)
+		{
+			//TODO: impelement
+		}
+	}
 }
